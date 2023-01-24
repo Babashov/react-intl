cd9a42dca680836ffd946dde93c0624e8f66c79c
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 17:02:00 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {IntlProvider, FormattedMessage} from 'react-intl'
+import { useState,useEffect } from 'react';
+
+  const messages = {
+    "az":{
+      title:'Salam Dunya',
+      description:'{count} yeni mesajiniz var'
+    },
+
+    "en-US":{
+      title:'Hello World',
+      description:'You have {count} new messages'
+    }
+  }
 
 function App() {
+  const isLocale = localStorage.getItem('locale')
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+
+   const [locale,setLocale] = useState(defaultLocale)
+
+  useEffect(()=>{
+    localStorage.setItem('locale',locale)
+  },[locale])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <FormattedMessage id='title'/>
+        <br/>
+        <FormattedMessage id='description' values={{count:3}}/>
+      </IntlProvider>
+
+      <br/><br/>
+
+      <button onClick={()=>setLocale('az')}>Azerbaijan</button>
+      <button onClick={()=>setLocale('en-US')}>English</button>
     </div>
   );
 }
